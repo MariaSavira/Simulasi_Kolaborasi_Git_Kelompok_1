ba34c8c97a569767b78a47c3bec5a881621c0cb2
MariaSavira <mariasavira2006@gmail.com>
2026-03-19 11:21:00 +0700

    Add Home dan About Page

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 65,
+            backgroundImage: AssetImage('assets/profile.jpg'),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Maria Savira',
+            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Project Manager Kelompok 1',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+          ),
+        ],
+      ),
+    );
+  }
+}

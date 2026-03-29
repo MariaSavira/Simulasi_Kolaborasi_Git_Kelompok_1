45283a2e385962772c747465dcaaf3ba8cd5cf6e
MariaSavira <mariasavira2006@gmail.com>
2026-03-29 09:21:04 +0700

    Tambah Landing Page, Tes Sasa

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:portofolio_sasa/pages/Portofolio_Sasa/about_page_sasa.dart';
-import 'package:portofolio_sasa/pages/Portofolio_Sasa/project_page_sasa.dart';
-import 'pages/Portofolio_Sasa/homepage_sasa.dart';
+import 'pages/Landing_Page/landing_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,55 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int _currentIndex = 0;
-
-  final List<Widget> _pages = [
-    HomePageSasa(),
-    AboutPageSasa(),
-    ProjectsPageSasa()
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Portfolio'),
-      ),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        selectedItemColor: Colors.teal,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'About',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.work),
-            label: 'Projects',
-          ),
-        ],
-      ),
+      home: LandingPage(),
     );
   }
 }
